3425a1cf10104aaba7b8aae21f9537e6646def0a
Sergej Novakovic <62727309+SergejNov@users.noreply.github.com>
2025-09-02 18:49:50 +0200

    Added files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import RecipeFinder from './RecipeFinder'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <RecipeFinder></RecipeFinder>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/RecipeFinder.jsx
@@ -0,0 +1,17 @@
+
+export default function RecipeFinder(){
+
+
+
+
+    return(
+        <div>
+            <div>
+               <h1>Search for a recipe!</h1>
+               <p>This is a mini aplication where you can search for a recipe 🥘</p>
+            </div>
+        <input type="text" />
+
+        </div>
+    )
+}
